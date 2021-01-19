e1ae3688af8cf962926d718d02cd00e7f81b615d
Sergi Obiols Olcina <sergiobi23@MacBook-Pro-de-Sergi.local>
2021-01-19 17:55:29 +0100

    big imporvements

--- app/src/main/java/com/example/frameworktesting/MainActivity.java
+++ app/src/main/java/com/example/frameworktesting/MainActivity.java
@@ -1,18 +1,25 @@
 package com.example.frameworktesting;
 
+import android.os.Bundle;
+import android.widget.ImageView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
-import com.example.mylibrary.ScanActivity;
+import com.example.mylibrary.ScanLayout;
 import com.example.mylibrary.ToastMessage;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ScanLayout scanLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        scanLayout = findViewById(R.id.scan_layout);
+        String URL = "https://static.wikia.nocookie.net/suits/images/d/da/S8_Harvey_Specter.jpg/revision/latest?cb=20180731195449";
+        scanLayout.setUrlImage(URL, ImageView.ScaleType.CENTER_CROP);
 
         ToastMessage.showToast(this, "hola");
     }
