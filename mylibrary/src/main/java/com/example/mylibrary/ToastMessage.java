dafe4b48b14186098a5b2f626df0c38ed82d13ae
Sergi Obiols Olcina <sergiobi23@MacBook-Pro-de-Sergi.local>
2021-01-19 17:10:47 +0100

    changed function

--- mylibrary/src/main/java/com/example/mylibrary/ToastMessage.java
+++ mylibrary/src/main/java/com/example/mylibrary/ToastMessage.java
@@ -1,11 +1,17 @@
 package com.example.mylibrary;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Context;
 import android.widget.Toast;
 
+import androidx.core.app.ActivityCompat;
+
 public class ToastMessage {
 
-    public static void showToast(Context context, String message){
-        Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
+    public static void showToast(Activity activity, String message){
+        ActivityCompat.requestPermissions(activity,
+                new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                1);
     }
 }
